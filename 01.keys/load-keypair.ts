e6ad2c987f0659f5edd46a35a895193441f63203
Yaroslav Ustinov <69744663+Yaroslaw07@users.noreply.github.com>
2024-05-31 02:26:24 +0200

    Finish combining projects

--- 01.keys/load-keypair.ts
+++
@@ -1,6 +0,0 @@
-import "dotenv/config";
-import { getKeypairFromEnvironment } from "@solana-developers/helpers";
-
-const keypair = getKeypairFromEnvironment("SECRET_KEY");
-
-console.log("Finished. The public key is: ", keypair.publicKey.toBase58());
